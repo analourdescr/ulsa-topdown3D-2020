113af39985241be31fe3820ce1ef3d6273bed65a
Ana Lourdes Cota Rascón <ana_lourdes98@hotmail.com>
2020-03-30 13:14:20 -0700

    actualizacion correcta de proyecto

---
+++ Assets/Scripts/Inventory/HealthPotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    [SerializeField]
+    Potion potion;
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        potion.Drink();
+        Debug.Log("Healt restored");
+    }
+}
---
+++ Assets/Scripts/Inventory/Potion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Potion", menuName = "Items/Potion", order = 1)]
+public class Potion : Consumable 
+{
+    [SerializeField]
+    int points;
+
+    public int Points { get => points;  }
+
+    public override void Drink()
+    {
+        base.Drink();
+        Debug.Log("Consumed");
+    }
+}
+
+
